d9bce2ed67d805ffb7c68a46489bdeb7f8a75fe2
Thomas Dos Santos <129770254+Thomdoss@users.noreply.github.com>
2024-11-27 15:21:10 -0500

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,30 @@
+const data = [
+   {
+      title: 'Iron',
+      desc: 'The starting rank for most new players. Focuses on learning the basics of the game.',
+      images: 'https://pbs.twimg.com/media/F3LFuyZXsAAsX-b.png',
+   },
+   {
+      title: 'Bronze',
+      desc: 'For players improving their fundamentals but still making common mistakes. A stepping stone to higher ranks.',
+      images:
+         'https://res.cloudinary.com/dv0epbifo/image/upload/v1698687339/guessmyrank_2023-10-30_23_03/lol/bronze-min_comnej_azcgbg.png',
+   },
+   {
+      title: 'Silver',
+      desc: 'Intermediate rank where players have a decent understanding of the game but need to work on consistency.',
+      images: 'https://buyboosting.fr/assets/imgs/lol/rank-icons/silver.png',
+   },
+   {
+      title: 'Gold',
+      desc: 'A competitive rank showing solid game knowledge and mechanics. Many players aim to reach Gold as a milestone.',
+      images:
+         'https://static.wikia.nocookie.net/leagueoflegends/images/7/78/Season_2023_-_Gold.png',
+   },
+   {
+      title: 'Platinum',
+      desc: 'Advanced rank. Players demonstrate strong skills and start understanding high-level strategies.',
+      images:
+         'https://static.wikia.nocookie.net/leagueoflegends/images/3/3b/Season_2022_-_Platinum.png',
+   },
+];
